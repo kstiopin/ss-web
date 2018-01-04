ec9445456e51631fadf447c200622f6a640c41bb
Salvation13 <salvation13xs@gmail.com>
2018-01-04 15:51:58 +0200

    Login: email input + validation

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -1,14 +1,37 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Dialog, FlatButton } from 'material-ui';
+import { Dialog, RaisedButton, TextField } from 'material-ui';
+
+import { validateEmail } from '../helpers';
 
 import Login from './Login';
 
 class App extends Component {
+  state = { email: '', errorText: '' }
+
+  setEmail = (ev, email) => this.setState({ email, errorText: '' })
+
+  handleAuth = () => {
+    const { email } = this.state;
+    if (email.length === 0) {
+      this.setState({ errorText: 'Email is required' });
+    } else {
+      const valid = validateEmail(email);
+      // console.log('Login', 'handleAuth', valid, email);
+      if (!valid) {
+        this.setState({ errorText: 'Email is invalid!' });
+      } else {
+        // handle auth
+      }
+    }
+  }
+
   render () {
+    const { errorText } = this.state;
+
     return (
-      <Dialog actions={ [<FlatButton label='Вход' primary onClick={ this.handleAuth } />] } modal open title='Авторизация'>
-        Форма входа
+      <Dialog actions={ [<RaisedButton label='Вход' primary onClick={ this.handleAuth } />] } modal open title='Авторизация'>
+        <TextField defaultValue='' errorText={ errorText } floatingLabelText='Email address' onChange={ this.setEmail } />
       </Dialog>);
   }
 }
