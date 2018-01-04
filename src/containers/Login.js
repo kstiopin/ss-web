e4207e57585949d9b5451b78d33676c2e95b9ee7
Salvation13 <salvation13xs@gmail.com>
2018-01-04 17:42:49 +0200

    Login: added password field

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -4,34 +4,45 @@ import { Dialog, RaisedButton, TextField } from 'material-ui';
 
 import { validateEmail } from '../helpers';
 
+// import { emailLoginAction } from '../actions';
+
 import Login from './Login';
 
 class App extends Component {
-  state = { email: '', errorText: '' }
+  state = { email: '', emailErrorText: '', pass: '', passErrorText: '' }
 
-  setEmail = (ev, email) => this.setState({ email, errorText: '' })
+  setEmail = (ev, email) => this.setState({ email, emailErrorText: '' })
+
+  setPass = (ev, pass) => this.setState({ pass, passErrorText: '' })
 
   handleAuth = () => {
-    const { email } = this.state;
-    if (email.length === 0) {
-      this.setState({ errorText: 'Email is required' });
+    const { email, pass } = this.state;
+    if (pass.length === 0) {
+      this.setState({ passErrorText: 'Password cannot be empty' });
     } else {
-      const valid = validateEmail(email);
-      // console.log('Login', 'handleAuth', valid, email);
-      if (!valid) {
-        this.setState({ errorText: 'Email is invalid!' });
+      if (email.length === 0) {
+        this.setState({ emailErrorText: 'Email is required' });
       } else {
-        // handle auth
+        const valid = validateEmail(email);
+        // console.log('Login', 'handleAuth', valid, email);
+        if (!valid) {
+          this.setState({ emailErrorText: 'Email is invalid!' });
+        } else {
+          // handle auth
+          // const { dispatch } = this.props;
+          // dispatch(emailLoginAction(email, pass));
+        }
       }
     }
   }
 
-  render () {
-    const { errorText } = this.state;
+  render() {
+    const { emailErrorText, passErrorText } = this.state;
 
     return (
-      <Dialog actions={ [<RaisedButton label='Вход' primary onClick={ this.handleAuth } />] } modal open title='Авторизация'>
-        <TextField defaultValue='' errorText={ errorText } floatingLabelText='Email address' onChange={ this.setEmail } />
+      <Dialog actions={ [<RaisedButton label='Вход' primary onClick={ this.handleAuth } />] } bodyClassName='login-form' modal open title='Авторизация'>
+        <TextField defaultValue='' errorText={ emailErrorText } floatingLabelText='Email address' onChange={ this.setEmail } />
+        <TextField defaultValue='' errorText={ passErrorText } floatingLabelText='Password' onChange={ this.setPass } />
       </Dialog>);
   }
 }
