b1e887fd47cbd56547c7e3c32ccfce33d0d7a67a
Salvation13 <salvation13xs@gmail.com>
2018-01-12 16:43:21 +0200

    save user to localStorage after successful login request

--- src/reducer.js
+++ src/reducer.js
@@ -1,10 +1,12 @@
 import * as types from './constants';
 
-const initialState = { user: {} };
+const user = (localStorage.getItem('user') === null) ? {} : JSON.parse(localStorage.getItem('user'));
+const initialState = { user };
 
 function mainReducer(state = initialState, action) {
   switch (action.type) {
     case types.EMAIL_LOGIN_SUCCESS:
+      localStorage.setItem('user', JSON.stringify(action.user));
       return { ...state, user: action.user };
     default:
       return state;
