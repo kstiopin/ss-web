9830428bdda411bb7061941bfebe227cdb263362
Salvation13 <salvation13xs@gmail.com>
2018-01-12 17:26:15 +0200

    show ContactsForm after we get user if we don't have some of details

---
+++ src/Components/ContactsForm.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Dialog, RaisedButton, TextField } from 'material-ui';
+
+class ContactsForm extends Component {
+  componentWillMount() {
+    const { user } = this.props;
+    const { firstname, lastname, phone, address } = user;
+    this.setState({ firstname, lastname, phone, address });
+  }
+
+  handleContactsSave = () => {
+    const { contactsSave } = this.props;
+    contactsSave(this.state);
+  }
+
+  setValue = (key, value) => {
+    const newState = this.state;
+    newState[key] = value;
+    this.setState(newState);
+  }
+
+  render() {
+    const { close } = this.props;
+    const { firstname, lastname, phone, address } = this.state;
+
+    return (
+      <Dialog actions={ [<RaisedButton label='Сохранить' primary onClick={ this.handleContactsSave } />] } bodyClassName='contacts-form' modal onRequestClose={ close } open title='Контактная информация'>
+        <TextField defaultValue={ firstname } floatingLabelText='Имя' onChange={ value => this.setValue('firstname', value) } />
+        <TextField defaultValue={ lastname } floatingLabelText='Фамилия' onChange={ value => this.setValue('lastname', value) } />
+        <TextField defaultValue={ phone } floatingLabelText='Телефон' onChange={ value => this.setValue('phone', value) } />
+        <TextField defaultValue={ address } floatingLabelText='Адрес' onChange={ value => this.setValue('address', value) } />
+      </Dialog>);
+  }
+}
+
+export default ContactsForm;
--- src/containers/App.js
+++ src/containers/App.js
@@ -5,8 +5,11 @@ import { connect } from 'react-redux';
 import { getUserDetailsAction } from '../actions';
 // Components
 import Login from './Login';
+import ContactsForm from '../components/ContactsForm';
 
 class App extends Component {
+  state = { showContacts: false }
+
   componentWillMount() {
     const { dispatch, user } = this.props;
     if (user.hasOwnProperty('id') && (user.id > 0)) {
@@ -18,7 +21,22 @@ class App extends Component {
     const { dispatch, user } = nextProps;
     if ((!this.props.user.hasOwnProperty('id') && user.hasOwnProperty('id')) || (this.props.user.id !== user.id)) {
       dispatch(getUserDetailsAction(user.id));
+    } else if (user.hasOwnProperty('id') && user.hasOwnProperty('firstname') && !this.props.user.hasOwnProperty('firstname')) {
+      this.checkUserDetails(user);
     }
+  }
+
+  checkUserDetails = (user) => {
+    const { firstname, lastname, phone, address } = user;
+    if (!firstname || !(firstname.length > 0) || !lastname || !(lastname.length > 0) || !phone || !(phone.length > 0) || !address || !(address.length > 0)) {
+      this.setState({ showContacts: true });
+    }
+  }
+
+  toggleContacts = () => this.setState(prevState => ({ showContacts: !prevState.showContacts }))
+
+  contactsSave = (details) => {
+    // save user details
   }
 
   render () {
@@ -29,10 +47,12 @@ class App extends Component {
     } else if (!user.hasOwnProperty('firstname')) {
       return null;
     }
+    const { showContacts } = this.state;
 
     return (
       <div>
         <h1>SmartSales</h1>
+        { showContacts && <ContactsForm contactsSave={ this.contactsSave } close={ this.toggleContacts } user={ user } /> }
       </div>);
   }
 }
