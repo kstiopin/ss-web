8b2abe890e901701e865959d777232a5a4e5ba4a
kstiopin <salvation13xs@gmail.com>
2019-03-14 21:58:36 +0200

    added REDUX_DEVTOOLS_EXTENSION_COMPOSE enhancer

--- src/main.js
+++ src/main.js
@@ -1,24 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { applyMiddleware, createStore } from 'redux';
+import { applyMiddleware, compose, createStore } from 'redux';
 import createSagaMiddleware from 'redux-saga';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 import mainReducer from './reducer';
 import mainSaga from './sagas';
-
 import App from './containers/App';
 
 const sagaMiddleware = createSagaMiddleware();
+const theme = createMuiTheme();
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const store = createStore(mainReducer, applyMiddleware(sagaMiddleware));
+const store = createStore(mainReducer, composeEnhancer(applyMiddleware(sagaMiddleware)));
 
 sagaMiddleware.run(mainSaga);
 
 ReactDOM.render(
   <Provider store={ store }>
-    <MuiThemeProvider>
+    <MuiThemeProvider theme={ theme }>
       <App />
     </MuiThemeProvider>
   </Provider>, document.getElementById('app'));
